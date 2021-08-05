b14c258b63669f1fe7f99c8a48ca4410bc8ecdda
Yong Lee <yong@windycitydevs.io>
2021-08-05 12:42:22 -0500

    adding seeding data

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -8,6 +8,7 @@ import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
 import 'dotenv/config';
+import { insertSeedData } from './seed-data';
 
 // set database url for project the || is for when the user has their own local database set up
 const databaseURL =
@@ -42,6 +43,12 @@ export default withAuth(
       adapter: 'mongoose',
       url: databaseURL,
       // TODO: Add data seeding here
+      async onConnect(keystone) {
+        console.log('connected to the database');
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
     },
     lists: createSchema({
       // schema items go in here
