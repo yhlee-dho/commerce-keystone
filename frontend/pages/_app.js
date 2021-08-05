fe9e1943d293c1c04cd58fd75e43f47bea633f0a
Yong Lee <yong@windycitydevs.io>
2021-08-05 16:21:07 -0500

    setting up Apollog client

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,8 @@
 import NProgress from 'nprogress';
 import Router from 'next/router';
+import { ApolloProvider } from '@apollo/client';
 import Page from '../components/Page';
+import withData from '../lib/withData';
 
 // CSS for NProcess progress bar
 // import 'nprogress/nprogress.css';
@@ -11,10 +13,25 @@ Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.start());
 
-export default function MyApp({ Component, pageProps }) {
-	return (
-		<Page>
-			<Component {...pageProps} />
-		</Page>
-	);
+function MyApp({ Component, pageProps, apollo }) {
+  console.log('----------------Apollo----------------');
+  console.log(apollo);
+  return (
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
+  );
 }
+
+MyApp.getInitialProps = async function (Component, ctx) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return pageProps;
+};
+
+export default withData(MyApp);
