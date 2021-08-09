7b536aecce8394230f925a74282b36aed50cff8b
Yong Lee <yong@windycitydevs.io>
2021-08-09 17:56:41 -0500

    rendering pagination

--- frontend/pages/products.js
+++ frontend/pages/products.js
@@ -1,10 +1,13 @@
+import Pagination from '../components/Pagination';
 import Products from '../components/Products';
 
 /* eslint-disable react/display-name */
 export default function OrderPage() {
   return (
     <div>
+      <Pagination page={1} />
       <Products />
+      <Pagination page={1} />
     </div>
   );
 }
