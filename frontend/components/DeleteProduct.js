1a291efa2fd74a69b2e7ae2f238ecd370feecb68
Yong Lee <yong@windycitydevs.io>
2021-08-09 17:02:55 -0500

    delete product using gql query

---
+++ frontend/components/DeleteProduct.js
@@ -0,0 +1,30 @@
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+
+const DELETE_PRODUCT_MUTATION = gql`
+  mutation DELETE_PRODUCT_MUTATION($id: ID!) {
+    deleteProduct(id: $id) {
+      id
+      name
+    }
+  }
+`;
+
+export default function DeleteProduct({ id, children }) {
+  const [deleteProduct, { loading }] = useMutation(DELETE_PRODUCT_MUTATION, {
+    variables: { id },
+  });
+  return (
+    <button
+      type="button"
+      disabled={loading}
+      onClick={() => {
+        if (confirm('Are you sure you want to delete this item?'));
+        // then go ahead and delete item
+        deleteProduct().catch((err) => alert(err.message));
+      }}
+    >
+      {children}
+    </button>
+  );
+}
