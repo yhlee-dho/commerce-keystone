0b8e4d6f17829d7925ad0d34ea46fe9353bd7e5c
Yong Lee <yong@windycitydevs.io>
2021-08-09 18:14:43 -0500

    dynamic routing for pagination

---
+++ frontend/pages/products/index.js
@@ -0,0 +1,18 @@
+import { useRouter } from 'next/dist/client/router';
+import Pagination from '../../components/Pagination';
+import Products from '../../components/Products';
+
+/* eslint-disable react/display-name */
+export default function OrderPage() {
+  const { query } = useRouter();
+  const page = parseInt(query.page);
+  console.log(typeof page);
+
+  return (
+    <div>
+      <Pagination page={page || 1} />
+      <Products />
+      <Pagination page={page || 1} />
+    </div>
+  );
+}
