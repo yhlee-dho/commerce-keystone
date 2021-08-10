2aab5c97c959267f3229e73256ac6eeb5efae959
Yong Lee <yong@windycitydevs.io>
2021-08-10 16:10:18 -0500

    signOut component to show when User &&

--- frontend/components/User.js
+++ frontend/components/User.js
@@ -1,6 +1,6 @@
 import { gql, useQuery } from '@apollo/client';
 
-export const CURRENT_USER_QUERY = gql`
+const CURRENT_USER_QUERY = gql`
   query {
     authenticatedItem {
       ... on User {
@@ -16,3 +16,5 @@ export function useUser() {
   const { data } = useQuery(CURRENT_USER_QUERY);
   return data?.authenticatedItem;
 }
+
+export { CURRENT_USER_QUERY };
