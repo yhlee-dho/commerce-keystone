986a435a7f95016765fbb5bf54b457eab8ca3ccc
Yong Lee <yong@windycitydevs.io>
2021-08-10 16:36:23 -0500

    sign up component

--- frontend/pages/signin.js
+++ frontend/pages/signin.js
@@ -1,10 +1,18 @@
+import styled from 'styled-components';
 import SignIn from '../components/SignIn';
+import SignUp from '../components/SignUp';
 
+const GridStyles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 2rem;
+`;
 /* eslint-disable react/display-name */
 export default function SignInPage() {
   return (
-    <div>
+    <GridStyles>
       <SignIn />
-    </div>
+      <SignUp />
+    </GridStyles>
   );
 }
