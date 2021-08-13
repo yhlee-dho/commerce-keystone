c0d025a6fe66d94176ca9ebe7074f3ea9a4eb921
Yong Lee <yong@windycitydevs.io>
2021-08-13 18:41:06 -0500

    search component using downShift

--- frontend/components/RemoveFromCart.js
+++ frontend/components/RemoveFromCart.js
@@ -21,10 +21,21 @@ const REMOVE_FROM_CART_MUTATION = gql`
   }
 `;
 
+function update(cache, payload) {
+  cache.evict(cache.identify(payload.data.deleteCartItem));
+}
+
 export default function RemoveFromCart({ id }) {
   const [removeFromCart, { loading }] = useMutation(REMOVE_FROM_CART_MUTATION, {
     variables: { id },
     // refetchQueries: [{ query: CURRENT_USER_QUERY }],
+    update,
+    // optimisticResponse: {
+    //   deleteCartItem: {
+    //     __typename: 'CartItem',
+    //     id,
+    //   },
+    // },
   });
   return (
     <BigButton
---
+++ frontend/components/Search.js
@@ -0,0 +1,108 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import { useLazyQuery } from '@apollo/client';
+import { resetIdCounter, useCombobox } from 'downshift';
+import gql from 'graphql-tag';
+import debounce from 'lodash.debounce';
+import { useRouter } from 'next/dist/client/router';
+import { DropDown, DropDownItem, SearchStyles } from './styles/DropDown';
+
+const SEARCH_PRODUCTS_QUERY = gql`
+  query SEARCH_PRODUCTS_QUERY($searchTerm: String!) {
+    searchTerms: allProducts(
+      where: {
+        OR: [
+          { name_contains_i: $searchTerm }
+          { description_contains_i: $searchTerm }
+        ]
+      }
+    ) {
+      id
+      name
+      photo {
+        image {
+          publicUrlTransformed
+        }
+      }
+    }
+    # Plate: allProducts(){}
+    # Chair: allProducts(){}
+  }
+`;
+
+export default function Search() {
+  const router = useRouter();
+  const [findItems, { loading, data, error }] = useLazyQuery(
+    SEARCH_PRODUCTS_QUERY,
+    {
+      fetchPolicy: 'no-cache',
+    }
+  );
+  // console.log(data);
+  const items = data?.searchTerms || [];
+
+  // time delay the network queries for search
+  const findItemsButChill = debounce(findItems, 400); // debounce from lodash
+
+  resetIdCounter();
+  const {
+    isOpen,
+    inputValue,
+    getMenuProps,
+    getInputProps,
+    getComboboxProps,
+    getItemProps,
+    highlightedIndex,
+  } = useCombobox({
+    items,
+    onInputValueChange() {
+      console.log('on Input Value Change!');
+      findItemsButChill({
+        variables: {
+          searchTerm: inputValue,
+        },
+      });
+    },
+    onSelectedItemChange({ selectedItem }) {
+      // console.log(selectedItem);
+      router.push({
+        pathname: `/product/${selectedItem.id}`,
+      });
+      console.log('on Selected Item Change!');
+    },
+    itemToString: (item) => item?.name || '',
+  }); // from downShift
+  return (
+    <SearchStyles>
+      <div {...getComboboxProps()}>
+        <input
+          {...getInputProps({
+            type: 'search',
+            placeholder: 'Search for an Item',
+            id: 'search',
+            className: loading ? 'loading' : '',
+          })}
+        />
+      </div>
+      <DropDown {...getMenuProps()}>
+        {isOpen &&
+          items.map((item, index) => (
+            <DropDownItem
+              key={item.id}
+              {...getItemProps({ item })}
+              highlighted={index === highlightedIndex}
+            >
+              <img
+                src={item.photo.image.publicUrlTransformed}
+                alt={item.name}
+                width="50"
+              />
+              {item.name}
+            </DropDownItem>
+          ))}
+        {isOpen && !items.length && !loading && (
+          <DropDownItem>Sorry, No items found for {inputValue}</DropDownItem>
+        )}
+      </DropDown>
+    </SearchStyles>
+  );
+}
