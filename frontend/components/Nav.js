e62cdd0b0a77c92dccdbf13167c5d45be0d64549
Yong Lee <yong@windycitydevs.io>
2021-08-13 15:38:21 -0500

    cart counter and animation

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -3,6 +3,7 @@ import NavStyles from './styles/NavStyles';
 import { useUser } from './User';
 import SignOut from './SignOut';
 import { useCart } from '../lib/cartState';
+import CartCount from './CartCount';
 
 export default function Nav() {
   const user = useUser();
@@ -19,6 +20,12 @@ export default function Nav() {
           <SignOut />
           <button type="button" onClick={openCart}>
             My Cart
+            <CartCount
+              count={user.cart.reduce(
+                (tally, cartItem) => tally + cartItem.quantity,
+                0
+              )}
+            />
           </button>
         </>
       )}
